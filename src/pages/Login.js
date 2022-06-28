20c759eed67e3f8361a1493d0ada983b6f349012
TFlexa <thiago_flexa@hotmail.com>
2022-06-28 18:34:36 -0300

    Requisito 02 correcao lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -67,8 +67,8 @@ class Login extends React.Component {
   }
 }
 
-Login.propTypes = { 
-  history: PropTypes.shape({push: PropTypes.func.isRequired}).isRequired,
+Login.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
 };
 
 export default Login;
