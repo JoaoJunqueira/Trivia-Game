88d9c0a75e1e25f0300ee1def16b3fdee8703eb9
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-06-28 22:11:07 -0300

    Requisito 4 finalizado

--- src/tests/helpers/Login.test.js
+++ src/tests/helpers/Login.test.js
@@ -4,34 +4,18 @@ import App from '../../App';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-const INITIAL_STATE = {
+const initialState = {
   player: {
     name: '',
-    assertions: 0,
-    score: 0,
-    gravatarEmail: '',
+    email: '',
   },
-};
-
-const tokenData = {
-  response_code: 0,
-  response_message: 'Token Generated Successfully!',
-  token: 'f00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6',
-};
-
-const mockFetch = (url) => {
-  if (url === `https://opentdb.com/api_token.php?command=request`) {
-    return Promise.resolve({
-      json: () => Promise.resolve(tokenData),
-    });
-  }
 };
 
 describe('Cobertura de testes da tela de Login', () => {
   afterEach(() => jest.clearAllMocks());
 
   test('Verifica se os campos de input são renderizados corretamente', () => {
-    renderWithRouterAndRedux(<App />);
+    renderWithRouterAndRedux(<App />, initialState, '/');
 
     const inputName = screen.getByPlaceholderText('Name');
     const inputEmail = screen.getByPlaceholderText('E-mail');
@@ -41,7 +25,7 @@ describe('Cobertura de testes da tela de Login', () => {
   });
 
   test("Verifica se ao digitar nos campos de input o botão 'Play' é habilitado", () => {
-    renderWithRouterAndRedux(<App />);
+    renderWithRouterAndRedux(<App />, initialState, '/');
 
     const inputName = screen.getByPlaceholderText('Name');
     const inputEmail = screen.getByPlaceholderText('E-mail');
@@ -56,7 +40,7 @@ describe('Cobertura de testes da tela de Login', () => {
   });
 
   test("Verifica se ao clicar no botão 'Play' o usuário é redirecionado para a página do jogo", async () => {
-    const { history } = renderWithRouterAndRedux(<App />);
+    const { history } = renderWithRouterAndRedux(<App />, initialState, '/');
 
     const inputName = screen.getByPlaceholderText('Name');
     const inputEmail = screen.getByPlaceholderText('E-mail');
@@ -70,7 +54,7 @@ describe('Cobertura de testes da tela de Login', () => {
   });
 
   test("Verifica se ao clicar no botão 'Settings' o usuário é redirecionado para a página de configurações", async () => {
-    const { history } = renderWithRouterAndRedux(<App />);
+    const { history } = renderWithRouterAndRedux(<App />, initialState, '/');
 
     const inputName = screen.getByPlaceholderText('Name');
     const inputEmail = screen.getByPlaceholderText('E-mail');
@@ -81,5 +65,59 @@ describe('Cobertura de testes da tela de Login', () => {
     userEvent.click(buttonSettings);
 
     await waitFor(() => expect(history.location.pathname).toBe('/settings'));
+  });
+
+  test("Verifica se ao clicar no botão 'Play' é feita uma requisição a API", async () => {
+    const url = 'https://opentdb.com/api_token.php?command=request';
+
+    const tokenData = {
+      response_code: 0,
+      response_message: 'Token Generated Successfully!',
+      token: 'f00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6',
+    };
+
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(tokenData),
+    });
+
+    renderWithRouterAndRedux(<App />, initialState, '/');
+
+    const inputName = screen.getByPlaceholderText('Name');
+    const inputEmail = screen.getByPlaceholderText('E-mail');
+    const buttonPlay = screen.getByRole('button', { name: 'Play' });
+
+    userEvent.type(inputName, 'Tryber');
+    userEvent.type(inputEmail, 'trybe@test.com');
+    userEvent.click(buttonPlay);
+
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalled();
+      expect(global.fetch).toHaveBeenCalledWith(url);
+    });
+  });
+
+  test('Verifica se o token recebido da API é salvo no localStorage', () => {
+    const tokenData = {
+      response_code: 0,
+      response_message: 'Token Generated Successfully!',
+      token: 'f00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6',
+    };
+
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(tokenData),
+    });
+
+    renderWithRouterAndRedux(<App />, initialState, '/');
+
+    const inputName = screen.getByPlaceholderText('Name');
+    const inputEmail = screen.getByPlaceholderText('E-mail');
+    const buttonPlay = screen.getByRole('button', { name: 'Play' });
+
+    userEvent.type(inputName, 'Tryber');
+    userEvent.type(inputEmail, 'trybe@test.com');
+    userEvent.click(buttonPlay);
+
+    const localStorageItem = localStorage.getItem('token');
+    expect(localStorageItem).toBe(tokenData.token);
   });
 });
