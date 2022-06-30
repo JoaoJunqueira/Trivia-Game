3d936eae80b9794108dc3f1d334c9bc94c6d3180
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-06-30 16:49:07 -0300

    requisito 13

--- src/pages/FeedBack.js
+++ src/pages/FeedBack.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class FeedBack extends React.Component {
   render() {
+    const { assertions } = this.props;
+    const three = 3;
     return (
       <div>
         <Header />
-        <div data-testid="feedback-text">FeedBack</div>
+        <div data-testid="feedback-text">
+          {
+            assertions < three ? <h6>Could be better...</h6> : <h6>Well Done!</h6>
+          }
+        </div>
       </div>
     );
   }
 }
 
-export default FeedBack;
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+FeedBack.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(FeedBack);
