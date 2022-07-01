3f450085d9d340205f9b5556e5bad4f990d9edff
julianaespindola <j.lespindolaa@gmail.com>
2022-06-30 20:40:50 -0400

    Requisito 19 parcialmente implementado

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
 
 class Ranking extends React.Component {
   state = {
     ranking: [],
   }
+
   goToHome = () => {
     const { history } = this.props;
     history.push('/');
@@ -20,18 +20,33 @@ class Ranking extends React.Component {
   componentDidMount = () => {
     const data = localStorage.getItem('ranking');
     const ranking = JSON.parse(data);
-    this.setState({ ranking });
+    const newRanking = ranking.sort((a, b) => {
+      const num = -1;
+      if (a.score > b.score) {
+        return num;
+      }
+      if (a.score < b.score) {
+        return 1;
+      }
+      return 0;
+    });
+    this.setState({ ranking: newRanking });
   }
 
   render() {
-    const { email, name, score } = this.props;
-    const index = 0;
+    const { ranking } = this.state;
     return (
       <div data-testid="ranking-title">
         <div>
-          <img alt={ name } src={ this.createSrcImg(email) } />
-          <h3 data-testid={ `player-name-${index}` }>{ name }</h3>
-          <p data-testid={ `player-score-${index}` }>{ score }</p>
+          {
+            ranking.map((item, index) => (
+              <div key={ index }>
+                <img alt={ item.name } src={ this.createSrcImg(item.email) } />
+                <h3 data-testid={ `player-name-${index}` }>{ item.name }</h3>
+                <p data-testid={ `player-score-${index}` }>{ item.score }</p>
+              </div>
+            ))
+          }
         </div>
         <button
           type="button"
@@ -45,13 +60,9 @@ class Ranking extends React.Component {
     );
   }
 }
-const mapStateToProps = (state) => ({
-  email: state.player.gravatarEmail,
-  name: state.player.name,
-  score: state.player.score,
-});
+
 Ranking.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
 };
 
-export default connect(mapStateToProps)(Ranking);
+export default Ranking;
