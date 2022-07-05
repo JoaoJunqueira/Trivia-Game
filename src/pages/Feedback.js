e4d65806f4aecba1066e3faa29983ad831e3f301
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-07-05 10:53:17 -0300

    Estilização feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 import { resetPlayerAction } from '../redux/actions';
+import '../Styles/styleFeedback.css';
 
 class Feedback extends React.Component {
   handleClick = () => {
@@ -23,39 +24,43 @@ class Feedback extends React.Component {
     return (
       <div>
         <Header />
-        <div data-testid="feedback-text">
-          <p>
-            Your Score:
-            {' '}
-            <span data-testid="feedback-total-score">{score}</span>
-          </p>
-          <p>
-            You got
-            {' '}
-            <span data-testid="feedback-total-question">{assertions}</span>
-            {' '}
-            { assertions > 1 ? 'questions right' : 'question right'}
-          </p>
-          {assertions < three ? (
-            <h3>Could be better...</h3>
-          ) : (
-            <h3>Well Done!</h3>
-          )}
+        <div className="card-feedback">
+          <div data-testid="feedback-text" className="feedback-container">
+            <p>
+              Your Score:
+              {' '}
+              <span data-testid="feedback-total-score">{score}</span>
+            </p>
+            <p>
+              You got
+              {' '}
+              <span data-testid="feedback-total-question">{assertions}</span>
+              {' '}
+              {assertions > 1 ? 'questions right' : 'question right'}
+            </p>
+            {assertions < three ? (
+              <h3>Could be better...</h3>
+            ) : (
+              <h3>Well Done!</h3>
+            )}
 
-          <button
-            data-testid="btn-play-again"
-            type="button"
-            onClick={ this.handleClick }
-          >
-            Play Again
-          </button>
-          <button
-            type="button"
-            data-testid="btn-ranking"
-            onClick={ this.goToRanking }
-          >
-            Ranking
-          </button>
+            <button
+              data-testid="btn-play-again"
+              type="button"
+              onClick={ this.handleClick }
+              className="btn btn-play-again"
+            >
+              Play Again
+            </button>
+            <button
+              type="button"
+              data-testid="btn-ranking"
+              onClick={ this.goToRanking }
+              className="btn btn-ranking"
+            >
+              Ranking
+            </button>
+          </div>
         </div>
       </div>
     );
